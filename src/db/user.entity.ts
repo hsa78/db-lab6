bf685657e7c12ba8feed5b5ee8938fa4c631d0c3
hsazarmsa <hsazarmsa@gmail.com>
2020-12-12 20:40:50 +0330

    add db folder with entities

---
+++ src/db/user.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, OneToMany } from 'typeorm';
+import BookEntity from './book.entity';
+@Entity()
+export default class UserEntity extends BaseEntity {
+    @OneToMany( type => BookEntity , book => book.user)
+    books: BookEntity[];
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 500 })
+    name: string;
+}
