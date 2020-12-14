22fa2e71251e36020916e707b6a815b74dda0a64
hsazarmsa <hsazarmsa@gmail.com>
2020-12-14 21:52:03 +0330

    add local gaurd to auth/login

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import {LocalAuthGuard} from './auth/local-auth.gaurd';
 
-@Controller('/hello')
+@Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
 }
