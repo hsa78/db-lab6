0b11a9d2d6aefff2c3520ef454f96f04d943f90a
hsazarmsa <hsazarmsa@gmail.com>
2020-12-19 22:37:33 +0330

    add services for insert and delete of item, task. insert of caategory and tag. controller api is also done. it is tested and works correctly

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,71 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Header, Body, Delete } from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { ApiResponse } from '@nestjs/swagger';
+import { Public } from 'src/auth/public';
+import CreateTaskDto from './dto/create-task.dto';
+import CreateCategoryDto from './dto/create-category.dto';
+import CreateTagDto from './dto/create-tag.dto';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+    constructor(private readonly todoService: TodoService) {}
+
+  @Post('task/insert')
+  @ApiResponse({ status:200, description:'create new task' }) 
+  @Header('Content-Type', 'application/json') 
+  @Public()
+  postTask( @Body() task: CreateTaskDto) {
+    return this.todoService.insertTask(task);
+  }
+
+  @Post('category/insert')
+  @ApiResponse({ status:200, description:'create new category' }) 
+  @Header('Content-Type', 'application/json') 
+  @Public()
+  postCategory( @Body() category: CreateCategoryDto) {
+    return this.todoService.insertCategory(category);
+  }
+
+  @Post('tag/insert')
+  @ApiResponse({ status:200, description:'create new tag' }) 
+  @Header('Content-Type', 'application/json') 
+  @Public()
+  postTag( @Body() tag: CreateTagDto) {
+    return this.todoService.insertTag(tag);
+  }
+
+  @Delete('task/delete')
+  @ApiResponse({ status:200, description:'delete existing task' }) 
+  @Header('Content-Type', 'application/json') 
+  @Public()
+  deleteTask( @Body() detail: {taskId: number}) {
+    return this.todoService.deleteTask(detail.taskId);
+  }
+
+  @Delete('item/delete')
+  @ApiResponse({ status:200, description:'delete existing item' }) 
+  @Header('Content-Type', 'application/json') 
+  @Public()
+  deleteItem( @Body() detail: {itemId: number}) {
+    return this.todoService.deleteItem(detail.itemId);
+  }
+// // 'getAll()' returns the list of all the existing users in the database
+//   @Get()
+//   @ApiResponse({ status:200, description:'show all users' }) 
+//   getAll() {
+//     return this.usersServices.getAllUsers();
+//   }
+
+// //'getBooks()' return all the books which are associated with the user 
+// // provided through 'userID' by the request  
+//   @Get('books')
+//   @ApiResponse({ status:200, description:'return all the books which are associated with the user' }) 
+//   @ApiQuery({
+//     name:'userID', 
+//     required:true, 
+//     type:Number, 
+//   })  
+//   getBooks( @Body('userID', ParseIntPipe) userID: number ) {
+//     return this.usersServices.getBooksOfUser(userID);
+//   }
+}
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,122 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
+import CreateTaskDto from './dto/create-task.dto';
+import TaskEntity from 'src/db/task.entity';
+import CategoryEntity from 'src/db/category.entity';
+import { isNullOrUndefined } from 'util';
+import TagEntity from 'src/db/tag.entity';
+import ItemEntity from 'src/db/item.entity';
+import { async } from 'rxjs';
+import CreateItemDto from './dto/create-item.dto';
+import CreateCategoryDto from './dto/create-category.dto';
+import CreateTagDto from './dto/create-tag.dto';
+import UserEntity from 'src/db/user.entity';
+import UpdateTaskDto from './dto/update-task.dto';
+import UpdateItemDto from './dto/update-item.dto';
+
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+    async insertTask(userDetails: CreateTaskDto): Promise<TaskEntity> {
+        const taskEntity: TaskEntity = TaskEntity.create();
+        taskEntity.title = userDetails.title;
+
+        let category = await CategoryEntity.findOne(userDetails.categoryId);
+        if (isNullOrUndefined(category)) 
+            throw new NotFoundException();
+        taskEntity.category = category;
+
+        if(userDetails.tagIds){
+            let tags: TagEntity[]; 
+            userDetails.tagIds.forEach(async tagId => {
+                let tag = await TagEntity.findOne(tagId);
+                if(isNullOrUndefined(tag))
+                    throw new NotFoundException();
+                tags.push(tag);
+            });
+            taskEntity.tags = tags;
+        }
+
+        
+
+        taskEntity.user = await UserEntity.findOne({where: {email: 'a@b.com'}});
+        taskEntity.compeleted = false;
+
+        await TaskEntity.save(taskEntity);
+
+        if(userDetails.items){
+            let items: ItemEntity[] = [];
+            userDetails.items.forEach(async item => {
+                let i = await this.insertItem(item, taskEntity);
+                items.push(i);
+            })
+        }
+
+        return taskEntity;
+      }
+
+      async insertItem(itemDetails: CreateItemDto, task: TaskEntity): Promise<ItemEntity>{
+        const itemEntity: ItemEntity = ItemEntity.create();
+
+        if(!itemDetails.title && !itemDetails.desc)
+            throw new BadRequestException(itemDetails, 'item should have one of tilte or desc');
+        
+        let title = itemDetails.title ? itemDetails.title : 'no title';
+        itemEntity.title = title;
+
+        if(itemDetails.desc){
+            itemEntity.desc = itemDetails.desc;
+        }
+        
+        itemEntity.compeleted = false;
+        itemEntity.task = task;
+
+        await ItemEntity.save(itemEntity);
+        return itemEntity;
+          
+      }
+
+      async insertCategory(categoryDetails: CreateCategoryDto): Promise<CategoryEntity>{
+        const categoryEntity: CategoryEntity = CategoryEntity.create();
+        categoryEntity.name = categoryDetails.name;
+        await CategoryEntity.save(categoryEntity);
+        return categoryEntity;
+      }
+
+      async insertTag(tagDetails: CreateTagDto): Promise<TagEntity>{
+        const tagEntity: TagEntity = TagEntity.create();
+        tagEntity.name = tagDetails.name;
+        await TagEntity.save(tagEntity);
+        return tagEntity;
+      }
+
+      async deleteItem(itemId: number): Promise<ItemEntity>{
+          let item = ItemEntity.findOne({where: {id:itemId}});
+          if(isNullOrUndefined(item))
+            throw new NotFoundException();
+          ItemEntity.delete({id:itemId});
+          return item;
+      }
+
+    async deleteTask(taskId: number): Promise<TaskEntity>{
+        let task = TaskEntity.findOne({where: {id:taskId}});
+        if(isNullOrUndefined(task))
+            throw new NotFoundException();
+        TaskEntity.delete({id:taskId});
+        return task;
+    }
+
+
+
+    //   async getAllUsers(): Promise<UserEntity[]> {
+    //     return await UserEntity.find();
+    //   }
+    //   async getBooksOfUser(userID: number): Promise<BookEntity[]> {
+    //     console.log(typeof(userID));
+    //     const user: UserEntity = await UserEntity.findOne({where: {id: userID}, relations: ['books']});
+    //     return user.books;
+    //   }
+    
+    //   async findOne(userId: number): Promise<UserEntity | undefined> {
+    //     return UserEntity.findOne({where:{id:userId}});
+    //   }
+}
