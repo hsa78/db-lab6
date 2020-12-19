ae72664e72b078647214295af73a2437dcb6bdbf
hsazarmsa <hsazarmsa@gmail.com>
2020-12-19 22:37:53 +0330

    delete book and genre modules

--- src/genre/dto/create-genre.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'; 
-import { IsNotEmpty } from "class-validator";
-
-export default class CreateGenreDto {
-    @ApiProperty({description:'type of genre'})
-    @IsNotEmpty()
-    readonly type: string;
-}
